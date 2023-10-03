ad7d9392e0fef188952bec0e9abdcb5ad34d5bdc
Allen Russell <garyrusselljr@gmail.com>
2023-10-03 11:23:59 -0600

    fix: encode token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::API
   before_action :authorized
 
   def encode_token(payload)
-    JWT.encode(payload, Rails.application.credentials.secret_key_base, true, algorithm: 'HS256')
+    JWT.encode(payload, Rails.application.credentials.secret_key_base)
   end
 
   def auth_header
